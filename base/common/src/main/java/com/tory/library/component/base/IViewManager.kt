7922babea64bfcf5f47eb10781d25968ebba1f83
徐涛Tory <xutao@theduapp.com>
2020-12-25 16:51:30 +0800

    StickyHeaders

---
+++ base/common/src/main/java/com/tory/library/component/base/IViewManager.kt
@@ -0,0 +1,12 @@
+package com.tory.library.component.base
+
+/**
+ * Author: tory
+ * Date: 12/24/20
+ * Email: xutao@theduapp.com
+ * Description:
+ */
+interface IViewManager<M: > {
+
+    fun getGridSize(): Int
+}
